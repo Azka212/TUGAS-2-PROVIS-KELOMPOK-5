e500d339c040667cf03385ba6eaa2e821e401e91
qalbuns <60817697+qalbuns@users.noreply.github.com>
2022-04-01 14:05:33 +0700

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\Azka Naufal N\\AppData\\Local\\Microsoft\\WindowsApps\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "E:\\flutter\\tugas\\flutter_tugas" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\Azka Naufal N\\AppData\\Local\\Microsoft\\WindowsApps\\flutter"
+  "PROJECT_DIR=E:\\flutter\\tugas\\flutter_tugas"
+  "FLUTTER_ROOT=C:\\Users\\Azka Naufal N\\AppData\\Local\\Microsoft\\WindowsApps\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=E:\\flutter\\tugas\\flutter_tugas\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=E:\\flutter\\tugas\\flutter_tugas"
+  "FLUTTER_TARGET=E:\\flutter\\tugas\\flutter_tugas\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=E:\\flutter\\tugas\\flutter_tugas\\.dart_tool\\package_config.json"
+)
